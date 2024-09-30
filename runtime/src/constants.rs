6e989fe847cfe3c59e1b9985c91887e949e0cfb0
Radek Bochenek <radekb@mac.com>
2024-09-30 19:40:23 +0200

    feat: replace Governance V1 with Polkadot OpenGov
    Squashed commit of the following:
    
    commit 6d848338706f9428555d3c3f68e7367ab8ce2a99
    Author: Radek Bochenek <radekb@mac.com>
    Date:   Mon Sep 30 19:39:03 2024 +0200
    
        feat: replace Governance V1 with OpenGov

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -9,68 +9,31 @@ pub mod scheduler {
 	pub const MAX_SCHEDULED_PER_BLOCK: u32 = 20;
 }
 
-pub mod council_collective {
+pub mod referenda {
 	use super::*;
-	// The time-out for council motions.
-	pub const MOTION_DURATION: BlockNumber = 20;
-	// Maximum number of proposals allowed to be active in parallel.
-	pub const MAX_PROPOSALS: u32 = 10;
-	// The maximum number of members supported by the pallet. Used for weight estimation.
-	//
-	// Benchmarks will need to be re-run and weights adjusted if this changes.
-	// This pallet assumes that dependents keep to the limit without enforcing it.
-	pub const MAX_MEMBERS: u32 = 15;
+	// The minimum amount to be used as a deposit for a public referendum proposal.
+	pub const SUBMISSION_DEPOSIT: Balance = 100_000_000_000;
+	// Maximum size of the referendum queue for a single track.
+	pub const MAX_QUEUED: u32 = 5;
+	// The number of blocks after submission that a referendum must begin being decided by. Once
+	// this passes, then anyone may cancel the referendum.
+	pub const UNDECIDING_TIMEOUT: BlockNumber = 20;
+	// Quantization level for the referendum wakeup scheduler. A higher number will result in fewer
+	// storage reads/writes needed for smaller voters, but also result in delays to the automatic
+	// referendum status changes. Explicit servicing instructions are unaffected.
+	pub const ALARM_INTERVAL: BlockNumber = 1;
 }
 
-pub mod technicalcommittee_collective {
+pub mod conviction_voting {
 	use super::*;
-	// The time-out for council motions.
-	pub const MOTION_DURATION: BlockNumber = 16;
-	// Maximum number of proposals allowed to be active in parallel.
-	pub const MAX_PROPOSALS: u32 = 4;
-	// The maximum number of members supported by the pallet. Used for weight estimation.
+	// The maximum number of concurrent votes an account may have.
 	//
-	// Benchmarks will need to be re-run and weights adjusted if this changes.
-	// This pallet assumes that dependents keep to the limit without enforcing it.
-	pub const MAX_MEMBERS: u32 = 10;
-}
-
-pub mod democracy {
-	use super::*;
-	// The period between a proposal being approved and enacted.
-	//
-	// It should generally be a little more than the unstake period to ensure that voting stakers
-	// have an opportunity to remove themselves from the system in the case where they are on the
-	// losing side of a vote.
-	pub const ENACTMENT_PERIOD: BlockNumber = 20;
-	// How often (in blocks) new public referenda are launched.
-	pub const LAUNCH_PERIOD: BlockNumber = 10;
-	// How often (in blocks) to check for new votes.
-	pub const VOTING_PERIOD: BlockNumber = 30;
+	// Also used to compute weight, an overly large value can lead to extrinsics with large weight
+	// estimation: see delegate for instance.
+	pub const MAX_VOTES: u32 = 100;
 	// The minimum period of vote locking.
 	//
 	// It should be no shorter than enactment period to ensure that in the case of an approval,
 	// those successful voters are locked into the consequences that their votes entail.
 	pub const VOTE_LOCKING_PERIOD: BlockNumber = 20;
-	// The minimum amount to be used as a deposit for a public referendum proposal.
-	pub const MINIMUM_DEPOSIT: Balance = 100_000_000;
-	// Indicator for whether an emergency origin is even allowed to happen. Some chains may want to
-	// set this permanently to false, others may want to condition it on things such as an upgrade
-	// having happened recently.
-	pub const INSTANT_ALLOWED: bool = true;
-	// Minimum voting period allowed for a fast-track referendum.
-	pub const FAST_TRACK_VOTING_PERIOD: BlockNumber = 10;
-	// Period in blocks where an external proposal may not be re-submitted after being vetoed.
-	pub const COOLOFF_PERIOD: BlockNumber = 30;
-	// The maximum number of votes for an account.
-	//
-	// Also used to compute weight, an overly big value can lead to extrinsic with very big weight:
-	// see delegate for instance.
-	pub const MAX_VOTES: u32 = 100;
-	// The maximum number of public proposals that can exist at any time.
-	pub const MAX_PROPOSALS: u32 = 20;
-	// The maximum number of deposits a public proposal may have at any time.
-	pub const MAX_DEPOSITS: u32 = 60;
-	// The maximum number of items which can be blacklisted.
-	pub const MAX_BLACKLISTED: u32 = 100;
 }

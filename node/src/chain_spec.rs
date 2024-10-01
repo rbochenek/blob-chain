cc8cf5780807684432f83556230c892b7b9d333b
Radek Bochenek <radekb@mac.com>
2024-10-01 22:43:30 +0200

    feat: replace Dave with Charlie in PoA authorities

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -71,7 +71,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		vec![
 			authority_keys_from_seed("Alice"),
 			authority_keys_from_seed("Bob"),
-			authority_keys_from_seed("Dave"),
+			authority_keys_from_seed("Charlie"),
 		],
 		// Sudo account
 		get_account_id_from_seed::<sr25519::Public>("Alice"),

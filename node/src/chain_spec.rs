37c578d785806d6d09828ab6a1b979ad544379e3
Radek Bochenek <radekb@mac.com>
2024-10-01 22:10:30 +0200

    feat: add Dave to PoA authorities

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -68,7 +68,11 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	.with_chain_type(ChainType::Local)
 	.with_genesis_config_patch(testnet_genesis(
 		// Initial PoA authorities
-		vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
+		vec![
+			authority_keys_from_seed("Alice"),
+			authority_keys_from_seed("Bob"),
+			authority_keys_from_seed("Dave"),
+		],
 		// Sudo account
 		get_account_id_from_seed::<sr25519::Public>("Alice"),
 		// Pre-funded accounts
